9e15bf43b0a49204d180c5c0926c8683b63ac999
Sanja Zunic <sanjazunic33@gmail.com>
2019-05-16 17:05:03 +0200

    Dodat paket common i klase DeviceProvider, TimeProvider

---
+++ IB_Project_Shell/src/main/java/ib/project/common/TimeProvider.java
@@ -0,0 +1,15 @@
+package ib.project.common;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TimeProvider implements Serializable {
+	private static final long serialVersionUID = -3301695478208950415L;
+
+    public Date now() {
+        return new Date();
+    }
+}

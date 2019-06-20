ccb6d644cf12b471c4360afb1d1b9defe94bd29f
Sanja Zunic <sanjazunic33@gmail.com>
2019-06-20 16:03:06 +0200

    Dodat paket repository i interface UserRepository

---
+++ IB_Project_Shell/src/main/java/ib/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package ib.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ib.project.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	User findByUsername(String username);
+}

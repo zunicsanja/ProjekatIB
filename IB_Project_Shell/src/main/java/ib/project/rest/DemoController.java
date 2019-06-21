534adccecec4d7a96c836e42d055530f14645a69
Sanja Zunic <sanjazunic33@gmail.com>
2019-06-21 13:51:16 +0200

    Dodat login page

--- IB_Project_Shell/src/main/java/ib/project/rest/DemoController.java
+++ IB_Project_Shell/src/main/java/ib/project/rest/DemoController.java
@@ -16,6 +16,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -108,4 +109,6 @@ public class DemoController {
 
 		return file;
 	}
+	
+	
 }

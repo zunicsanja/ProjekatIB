df17e4161efb2d232cf0031f52ccc4770865b0d7
Sanja Zunic <sanjazunic33@gmail.com>
2019-06-23 17:20:01 +0200

    Izmenjen izgled za login

--- IB_Project_Shell/src/main/java/ib/project/config/WebSecurityConfig.java
+++ IB_Project_Shell/src/main/java/ib/project/config/WebSecurityConfig.java
@@ -72,20 +72,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     //Definisemo prava pristupa odredjenim URL-ovima
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-        		//komunikacija izmedju klijenta i servera je stateless
-                .sessionManagement().sessionCreationPolicy( SessionCreationPolicy.STATELESS ).and()
-                //za neautorizovane zahteve posalji 401 gresku
-                .exceptionHandling().authenticationEntryPoint( restAuthenticationEntryPoint ).and()
-                .authorizeRequests()
-                //svim korisnicima dopusti da pristupe putanjama /auth/**
-                .antMatchers("/auth/**").permitAll()
-                //svaki zahtev mora biti autorizovan
-                .anyRequest().authenticated().and()
-                //presretni svaki zahtev filterom
-                .addFilterBefore(new TokenAuthenticationFilter(tokenHelper, jwtUserDetailsService), BasicAuthenticationFilter.class);
+    	 http
+ 		//komunikacija izmedju klijenta i servera je stateless
+         .sessionManagement().sessionCreationPolicy( SessionCreationPolicy.STATELESS ).and()
+         //za neautorizovane zahteve posalji 401 gresku
+         .exceptionHandling().authenticationEntryPoint( restAuthenticationEntryPoint ).and()
+         .authorizeRequests()
+         //svim korisnicima dopusti da pristupe putanjama /auth/**
+         .antMatchers("/auth/**").permitAll()
+         //svaki zahtev mora biti autorizovan
+         .anyRequest().authenticated().and()
+         //presretni svaki zahtev filterom
+         .addFilterBefore(new TokenAuthenticationFilter(tokenHelper, jwtUserDetailsService), BasicAuthenticationFilter.class);
 
-        http.csrf().disable();
+    	 http.csrf().disable();
     }
 
 
---
+++ IB_Project_Shell/src/main/java/ib/project/service/impl/UserDetails.java
@@ -0,0 +1,49 @@
+package ib.project.service.impl;
+
+import ib.project.model.User;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class UserDetails implements org.springframework.security.core.userdetails.UserDetails{
+	private User user;
+    public UserDetails(User user) {
+        this.user = user;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getAuthorities().stream().map(authority -> new SimpleGrantedAuthority(authority.getName().toString())).collect(Collectors.toList());
+    }
+    public Long getId() {
+        return user.getId();
+    }
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    public User getUserDetails() {
+        return user;
+    }
+}
